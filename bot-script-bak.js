697c09a2bb6f890e0e1aa67fbca98c90fcf0c285
t <t@tde-MacBook-Air.local>
2020-12-07 22:17:56 +0800

    first commit

---
+++ bot-script-bak.js
@@ -0,0 +1,59 @@
+var botui = new BotUI('help-bot');
+
+botui.message.add({
+  delay: 500,
+  loading: true,
+  content: 'Hi! Welcome to my website'
+}).then(function () {
+  return botui.message.add({
+    delay: 500,
+    loading: true,
+    content: 'How can I help?'
+  });
+})
+.then(function () {
+  return botui.action.button({
+    action: [
+      {
+        text: 'What are your opening hours?',
+        value: 'hours'
+      },
+      {
+        text: 'What do you do?',
+        value: 'do'
+      }
+    ]
+  });
+})
+// .then(function (res) {
+//   var message;
+
+//   if (res.value === "hours") {
+//     message = 'That’s a good one! This is a website, it’s always open.';
+//   }
+//   else if (res.value === "do") {
+//     message = 'I’m a product-focused Scrum master<br><br>I also have a keen interest in chatbots and web analytics';
+//   }
+
+//   return botui.message.add({
+//     type: 'html',
+//     delay: 1000,
+//     loading: true,
+//     content: message
+//   });
+// }).then(function (index) {
+//   return botui.action.button({
+//     action: [
+//       {
+//         text: 'Cool!',
+//         value: 'cool'
+//       }
+//     ]
+//   });
+// }).then(function (index) {
+//   return botui.message.add({
+//     delay: 1000,
+//     loading: true,
+//     content: 'I know! Thanks.'
+//   });
+// });
--- bot-script.js
+++ bot-script.js
@@ -1,57 +1,209 @@
 var botui = new BotUI('help-bot');
+var res_email = '';
 
-botui.message.add({
-  delay: 500,
-  loading: true,
-  content: 'Hi! Welcome to my website'
-}).then(function () {
-  return botui.message.add({
-    delay: 500,
-    loading: true,
-    content: 'How can I help?'
-  });
-}).then(function () {
-  return botui.action.button({
-    action: [
-      {
-        text: 'What are your opening hours?',
-        value: 'hours'
-      },
-      {
-        text: 'What do you do?',
-        value: 'do'
-      }
-    ]
-  });
-}).then(function (res) {
-  var message;
+greetings()
+  .then(function (res) {
+    if (res.value === "what_can_you_do") {
+      what_can_you_do()
+      .then(function (res) { // will be called when a button is clicked.
+        if (res.value == 'password_reset') {
+          entering_email_id()
+            .then(function () {
+              verification_method()
+                .then(function (res) {
+                  if (res.value == 'phone_call') {
+                    phone_call()
+                      .then(function () {
+                        reset_success()
+                        .then(function (res) { // will be called when a button is clicked.
+                          if (res.value == 'false') {
+                            return goodbye();
+                          } else if (res.value == 'true') {
+                            return what_can_you_do();
+                          }
+                        });
+                      })
+                  }
+                })
+            });
+        }
+      });
+    } else {
+      goodbye();
+    }
+  })
 
-  if (res.value === "hours") {
-    message = 'That’s a good one! This is a website, it’s always open.';
+
+  function goodbye() {
+    botui.message.bot({
+      delay: 500,
+      content: 'Okay! Have a nice day! <i class="far fa-smile-wink"></i>'
+    })
   }
-  else if (res.value === "do") {
-    message = 'I’m a product-focused Scrum master<br><br>I also have a keen interest in chatbots and web analytics';
+
+  function what_can_you_do() {
+    return botui.action.button({
+      action: [
+        {
+          icon: 'search-plus',
+          text: 'Trouble Shooting',
+          value: 'trouble_shooting'
+        },
+        {
+          icon: 'ticket',
+          text: 'Create Ticket',
+          value: 'create_ticket'
+        },
+        {
+          icon: 'unlock-alt',
+          text: 'Password Reset',
+          value: 'password_reset'
+        },
+      ]
+    }
+    )
   }
 
+  function verification_method(){
+    return botui.message.add({
+      type: 'html',
+      delay: 500,
+      loading: true,
+      content: 'kindly choose your verification method'
+    }).then(function (){
+      return botui.action.button({
+        action: [
+          {
+            text: 'SMS',
+            value: 'sms'
+          },
+          {
+            text: 'Phone Call',
+            value: 'phone_call'
+          }
+        ]
+      });
+    })
+  }
+
+function anything_else() {
   return botui.message.add({
     type: 'html',
-    delay: 1000,
+    delay: 500,
     loading: true,
-    content: message
-  });
-}).then(function (index) {
-  return botui.action.button({
-    action: [
-      {
-        text: 'Cool!',
-        value: 'cool'
-      }
-    ]
-  });
-}).then(function (index) {
+    content: 'Is there anything else i could help with?'
+  }).then(function () {
+    return botui.action.button({
+      action: [
+        {
+          text: 'Yes',
+          value: 'true'
+        },
+        {
+          text: 'No',
+          value: 'false'
+        }
+      ]
+    })
+  })
+}
+
+function entering_email_id(){
   return botui.message.add({
-    delay: 1000,
+    type: 'html',
+    delay: 500,
     loading: true,
-    content: 'I know! Thanks.'
+    content: 'kindly please confirm your identity by entering your email id'
+  })
+    .then(function () {
+      return botui.action.text({
+        delay: 500,
+        loading: true,
+        action: {
+          sub_type: 'email',
+          placeholder: 'someone@somesite.com'
+        },
+      })
+    }).then(function (res_email) {
+      return botui.message.add({
+        type: 'html',
+        delay: 500,
+        loading: true,
+        content: 'your email is '+res_email.value+'. Confirm?'
+      })
+    }).then(function (){
+      return botui.action.button({
+        action: [
+          {
+            text: 'Yes',
+            value: 'true'
+          },
+          {
+            text: 'No',
+            value: 'false'
+          }
+        ]
+      }).then(function(res){
+        if(res.value == 'true'){
+          email = res_email.value;
+          return email
+        }else if(res.value == 'false'){
+          return entering_email_id();
+        }
+      })
+    });
+}
+
+function greetings(){
+  return botui.message.add({
+    type: 'html',
+    delay: 500,
+    loading: true,
+    content: 'Hello Visitor! I am IT Helpdesk <b>BOT</b>'
+  })
+    .then(function () {
+      return botui.message.add({
+        delay: 500,
+        loading: true,
+        content: 'To know more about how i can help <br> please click below:'
+      });
+    })
+    .then(function (index) {
+      return botui.action.button({
+        action: [
+          {
+            text: 'What can you do?',
+            value: 'what_can_you_do'
+          }
+        ]
+      });
+    })
+}
+
+function reset_success(){
+  return botui.message.add({
+    type: 'html',
+    delay: 500,
+    loading: true,
+    content: 'Your password is successfully reset. <br> Your new password is @cuvate@123'
+  })
+  .then(function () {
+  return botui.message.add({
+    type: 'html',
+    delay: 500,
+    loading: true,
+    content: 'Please change your password upon login'
+  }).then(function(){
+    return anything_else()
   });
 });
+}
+
+function phone_call(){
+  return botui.message.add({
+    type: 'html',
+    delay: 500,
+    loading: true,
+    content: 'Calling you on your registered mobile. <br> Please press \"#\" key when you are asked to verify'
+  })
+}
